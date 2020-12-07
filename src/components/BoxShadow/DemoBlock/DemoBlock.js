f336cccc4f248d8a56b7e1b5ce344bcc43bc3087
anton <kiyloanton@gmail.com>
2020-12-07 20:32:26 +0300

    added ability to copy css code

--- src/components/BoxShadow/DemoBlock/DemoBlock.js
+++ src/components/BoxShadow/DemoBlock/DemoBlock.js
@@ -1,13 +1,32 @@
+import { useRef } from 'react'
 import './DemoBlock.css'
 
 const DemoBlock = ({shadowRange}) => {
+  const boxShadowCss = useRef()
   const color = `rgba(${parseInt(shadowRange.color.slice(1, 3), 16)}, ${parseInt(shadowRange.color.slice(3, 5), 16)}, ${parseInt(shadowRange.color.slice(5, 7), 16)}, ${shadowRange.opacity})`
+  const copyCss = () => {
+    let range = new Range();
+
+    range.setStart(boxShadowCss.current, 0)
+    range.setEnd(boxShadowCss.current, 3)
+
+    document.getSelection().addRange(range)
+    document.execCommand('copy')
+    document.getSelection().removeAllRanges()
+  } 
 
   return (
     <div
       className='demo-block'
       style={{ boxShadow: `${shadowRange.horizontalShift}px ${shadowRange.verticalShift}px ${shadowRange.blur}px ${shadowRange.spread}px ${color}` }}
-    ></div>
+    >
+      <div ref={boxShadowCss} className='box-shadow-css'>
+        <p>{`box-shadow: ${shadowRange.horizontalShift}px ${shadowRange.verticalShift}px ${shadowRange.blur}px ${shadowRange.spread}px ${color};`}</p>
+        <p>{`-moz-box-shadow: ${shadowRange.horizontalShift}px ${shadowRange.verticalShift}px ${shadowRange.blur}px ${shadowRange.spread}px ${color};`}</p>
+        <p>{`-webkit-box-shadow: ${shadowRange.horizontalShift}px ${shadowRange.verticalShift}px ${shadowRange.blur}px ${shadowRange.spread}px ${color};`}</p>
+      </div>
+      <button onClick={copyCss}>Copy</button>
+    </div>
   )
 }
 
